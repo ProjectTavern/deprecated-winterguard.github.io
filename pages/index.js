6c1790ce5d1cc21991f20596302c2ff4de8324b2
winterguard <aliasbonfire@gmail.com>
2020-09-24 20:29:54 +0900

    add key for list

--- pages/index.js
+++ pages/index.js
@@ -10,21 +10,25 @@ export default function Home() {
   };
   const indexCards = [
     {
+      key: 'index-javascript',
       title: '자바스크립트',
       description: '프론트엔드 기술로 자바스크립트를 사용하고 있습니다.',
       href: 'https://google.com'
     },
     {
+      key: 'index-algorithm',
       title: '알고리즘',
       description: '기능 구현을 위한 알고리즘을 학습하고 있습니다.',
       href: '#'
     },
     {
+      key: 'index-portfolio',
       title: '포트폴리오',
       description: '이제까지 진행해온 프로젝트입니다.',
       href: '#'
     },
     {
+      key: 'index-about',
       title: 'About',
       description: '간략한 소개입니다.',
       href: '#'
