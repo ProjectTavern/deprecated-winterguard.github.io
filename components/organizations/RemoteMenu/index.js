09f841f982d5a13fba77bfcb0ca053e16570c1e6
winterguard <aliasbonfire@gmail.com>
2020-09-29 19:50:33 +0900

    create remoteMenuButton using useState

--- components/organizations/RemoteMenu/index.js
+++ components/organizations/RemoteMenu/index.js
@@ -1,15 +1,26 @@
+import { useState } from 'react'
+
 import Link from 'next/link'
-import { StyledRemoteMenuList } from './private.RemoteMenu.styled'
+import { StyledRemoteMenuContainer, StyledRemoteMenuButton, StyledRemoteMenuList } from './private.RemoteMenu.styled'
 
-const RemoteMenu = ({ indexCards }) =>
-  (<StyledRemoteMenuList>
-    <li>
-      <Link href="/">Home</Link>
-    </li>
-    {indexCards.map(({ key, title, href }) =>
-      (<li key={key}>
-        <Link href={href}>{ title }</Link>
-      </li>))}
-  </StyledRemoteMenuList>)
+const RemoteMenu = ({ indexCards }) =>{
+  const [openMenuList, setOpenMenuList] = useState(true);
 
+  const RemoteMenuList = () => (openMenuList &&
+    (<StyledRemoteMenuList>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      {indexCards.map(({ key, title, href }) =>
+        (<li key={key}>
+          <Link href={href}>{ title }</Link>
+        </li>))}
+    </StyledRemoteMenuList>))
+
+  return (
+    <StyledRemoteMenuContainer>
+    <StyledRemoteMenuButton onClick={() => setOpenMenuList(!openMenuList)}>버튼</StyledRemoteMenuButton>
+    <RemoteMenuList />
+  </StyledRemoteMenuContainer>)
+}
 export default RemoteMenu
--- components/organizations/RemoteMenu/private.RemoteMenu.styled.js
+++ components/organizations/RemoteMenu/private.RemoteMenu.styled.js
@@ -1,14 +1,28 @@
 import styled from 'styled-components'
 
-const StyledRemoteMenuList = styled.ul`
+const StyledRemoteMenuContainer = styled.div`
   position: sticky;
-  display: flex;
-  height: 30px;
   top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const StyledRemoteMenuButton = styled.div`
+  width: 80px;
+  height: 80px;
+`
+
+const StyledRemoteMenuList = styled.ul`
+  flex-grow: 1;
+  display: flex;
+  height: 80px;
   align-items: center;
   justify-content: space-around;
 `
 
 export {
+  StyledRemoteMenuContainer,
+  StyledRemoteMenuButton,
   StyledRemoteMenuList
 }
