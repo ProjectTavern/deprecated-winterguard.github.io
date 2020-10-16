72fa1a3db9fc78833cb5dff8769104ad05557cc6
winterguard <aliasbonfire@gmail.com>
2020-10-16 10:42:18 +0900

    change draggable remote button

--- components/organizations/RemoteMenu/index.js
+++ components/organizations/RemoteMenu/index.js
@@ -1,38 +1,144 @@
-import { useState } from "react";
+import { useState, useRef, useCallback } from "react";
 
 import Link from "next/link";
 import {
   StyledRemoteMenuContainer,
+  StyledRemoteMenuButtonContainer,
+  StyledRemoteMenuControlButton,
   StyledRemoteMenuButton,
   StyledRemoteMenuList,
   StyledRemoteMenuListItem,
 } from "./private.RemoteMenu.styled";
 
-const RemoteMenu = ({ indexCards }) => {
-  const [openMenuList, setOpenMenuList] = useState(true);
-
-  const RemoteMenuList = () => (
-    <StyledRemoteMenuList>
-      <StyledRemoteMenuListItem>
-        <Link href="/">Home</Link>
+const RemoteMenuList = ({ indexCards }) => (
+  <StyledRemoteMenuList>
+    <StyledRemoteMenuListItem>
+      <Link href="/">Home</Link>
+    </StyledRemoteMenuListItem>
+    {indexCards.map(({ key, title, href }) => (
+      <StyledRemoteMenuListItem key={key}>
+        <Link href={href}>{title}</Link>
       </StyledRemoteMenuListItem>
-      {indexCards.map(({ key, title, href }) => (
-        <StyledRemoteMenuListItem key={key}>
-          <Link href={href}>{title}</Link>
-        </StyledRemoteMenuListItem>
-      ))}
-    </StyledRemoteMenuList>
-  );
+    ))}
+  </StyledRemoteMenuList>
+);
+
+const RemoteMenuMoveButton = (props) => {
+  // UI 상태의 값이 아니므로 ref에 저장하는 것이 좀 더 좋을 것 같습니다.
+  const { moveTargetElement } = props;
+  const hold = useRef(false)
+  const positionGap = useRef({ x: 0, y: 0 })
+
+  const handleMouseMove = useCallback((event) => {
+    event.preventDefault();
+
+    if (!hold.current) {
+      return false;
+    }
+
+    const mousePosition = {
+      x: event.pageX,
+      y: event.pageY
+    }
+    const willBeTargetPosition = {
+      x: mousePosition.x - positionGap.current.x,
+      y: mousePosition.y - positionGap.current.y
+    }
+
+    moveTargetElement.current.style.left = `${willBeTargetPosition.x}px`
+    moveTargetElement.current.style.top = `${willBeTargetPosition.y}px`
+  }, [])
+
+  const handleMouseDown = (event) => {
+    event.preventDefault();
+
+    if (!moveTargetElement.current) {
+      return false;
+    }
+
+    const mousePosition = {
+      x: event.pageX,
+      y: event.pageY
+    }
+    const targetPosition = moveTargetElement.current.getBoundingClientRect();
+
+    positionGap.current = {
+      x: mousePosition.x - targetPosition.x,
+      y: mousePosition.y - targetPosition.y
+    }
+
+    hold.current = true;
+
+    document.addEventListener('mousemove', handleMouseMove);
+  }
+
+
+
+  const handleRemoveController = (event) => {
+    event.preventDefault();
+
+    hold.current = false;
+    document.removeEventListener('mousemove', handleMouseMove);
+  }
+
+  return (
+    <StyledRemoteMenuControlButton
+      onMouseDown={(event) => handleMouseDown(event)}
+      onMouseUp={(event) => handleRemoveController(event)}
+      {...props}
+    >
+      이동
+    </StyledRemoteMenuControlButton>
+  )
+}
+
+
+const RemoteMenuFoldButton = (props) => {
+  return (
+    <StyledRemoteMenuControlButton {...props}>
+      X
+    </StyledRemoteMenuControlButton>
+  )
+}
+
+const RemoteMenu = ({ indexCards }) => {
+  const [openMenuList, setOpenMenuList] = useState(false);
+  const [openRemoteMenu, setOpenRemoteMenu] = useState(true);
+  const containerElement = useRef(null);
+
+  const toggleMenuList = () => {
+    if (!openRemoteMenu) {
+      return false;
+    }
+    setOpenMenuList(!openMenuList)
+  }
+
+  const toggleRemoteMenu = () => {
+    setOpenMenuList(false);
+    setOpenRemoteMenu(!openRemoteMenu);
+  }
 
   return (
     <StyledRemoteMenuContainer
-      className={openMenuList ? "remote-menu--open" : "remote-menu--close"}
+      ref={containerElement}
+      className={[
+        openMenuList ? "remote-menu-list--open" : "remote-menu-list--close",
+        openRemoteMenu ? "remote-menu--open" : "remote-menu--close"]}
     >
-      <StyledRemoteMenuButton onClick={() => setOpenMenuList(!openMenuList)}>
-        버튼
-      </StyledRemoteMenuButton>
-      <RemoteMenuList />
+      <StyledRemoteMenuButtonContainer>
+        <RemoteMenuMoveButton moveTargetElement={containerElement} />
+        <StyledRemoteMenuButton
+          onClick={() => toggleMenuList()}
+        >
+          버튼
+        </StyledRemoteMenuButton>
+        <RemoteMenuFoldButton
+          onClick={() => toggleRemoteMenu()}
+        />
+      </StyledRemoteMenuButtonContainer>
+      <RemoteMenuList indexCards={indexCards} />
     </StyledRemoteMenuContainer>
   );
 };
+
 export default RemoteMenu;
--- components/organizations/RemoteMenu/private.RemoteMenu.styled.js
+++ components/organizations/RemoteMenu/private.RemoteMenu.styled.js
@@ -1,61 +1,79 @@
-import Link from "next/link";
 import styled from "styled-components";
-
-const StyledRemoteMenuLink = styled(Link)`
-  display: block;
-  height: 100%;
-`;
 
 const StyledRemoteMenuContainer = styled.div`
   position: fixed;
-  top: 0;
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-between;
   transition: width ease-out 0.25s;
+  width: 240px;
+  border: 1px solid #000;
+  border-radius: 8px;
   overflow: hidden;
+  background-color: rgba(0, 0, 0, 0.07);
+  &.remote-menu-list--open {
+    max-height: 500px;
+    overflow-y: scroll;
+  }
+  &.remote-menu-list--close {
+    height: 1.5rem;
+  }
   &.remote-menu--open {
-    width: 32px;
-    height: 32px;
+    
   }
   &.remote-menu--close {
-    width: 100%;
+    width: 65px;
   }
 `;
 
-const StyledRemoteMenuButton = styled.div`
-  max-width: 32px;
-  min-width: 32px;
-  width: 32px;
-  height: 32px;
-  border-radius: 100%;
-  border: 1px solid #000;
-`;
-
-const StyledRemoteMenuList = styled.ul`
-  flex-grow: 1;
+const StyledRemoteMenuButtonContainer = styled.div`
+  position: sticky;
   display: flex;
   align-items: center;
-  justify-content: space-around;
-  height: 80px;
+  top: 0;
+  width: 100%;
+  height: 1.5rem;
+  text-align: center;
+  border-bottom: 1px solid #000;
+`;
+
+const StyledRemoteMenuControlButton = styled.button`
+  font-size: 0;
+  height: 100%;
+  background-color: red;
+`
+
+const StyledRemoteMenuButton = styled.button`
+  flex-grow: 1;
+`
+
+const StyledRemoteMenuList = styled.ul`
+  width: 100%;
 `;
 
 const StyledRemoteMenuListItem = styled.li`
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 100%;
   height: 100%;
   background: rgab(125, 234, 57, 0.5);
   &:hover {
     background: rgba(125, 234, 57, 0.3);
   }
+  a {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    padding: 8px 16px;
+  }
 `;
 
 export {
   StyledRemoteMenuContainer,
+  StyledRemoteMenuButtonContainer,
+  StyledRemoteMenuControlButton,
   StyledRemoteMenuButton,
   StyledRemoteMenuList,
   StyledRemoteMenuListItem,
-  StyledRemoteMenuLink,
 };
