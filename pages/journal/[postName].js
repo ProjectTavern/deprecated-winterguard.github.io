728ac4df00d22203742e9162e0b34221890864da
winterguard <aliasbonfire@gmail.com>
2020-10-20 21:39:39 +0900

    개발 환경과 깃헙 배포 환경이 달라서 분기 코드 생성

--- pages/journal/[postName].js
+++ pages/journal/[postName].js
@@ -1,25 +1,38 @@
 import { getPosts, getPost } from '@/preAPI/posts'
 import { Post } from "@/components/templates";
+import { encode } from 'punycode';
 
 const pageConfig = {
   categoryURI: 'journal'
 }
 
 const JournalPost = ({ post }) => {
-
-  return (process.browser &&
+  return (
     <React.Fragment>
-      <h3>{post.title}</h3>
-      <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
+      <h1>{Journal}</h1>
+      <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
     </React.Fragment>
   );
 };
+
+/**
+ * 배포 환경에 따라 path 문자열을 인코딩할지를 결정하는 함수
+ * 개발환경: 인코딩을 안했을 경우 제대로 인식하지 않음
+ * @param {String} target 
+ */
+const parseStaticPathByEnvironment = (target) => {
+  const environment = process.env.NODE_ENV;
+  const isProduction = environment === 'production';
+  const filteredTargetName = target.replace('.md', '');
+
+  return isProduction ? filteredTargetName : encodeURI(filteredTargetName);
+}
 
 const getStaticPaths = async () => {
   const allPosts = await getPosts(pageConfig);
   const paths = allPosts.map(
     ({ filename }) => ({
-      params: { postName: filename.replace('.md', '') }
+      params: { postName: parseStaticPathByEnvironment(filename) }
     })
   );
 
