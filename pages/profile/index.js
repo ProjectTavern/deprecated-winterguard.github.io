28afdec16420c3cff6939c662de2c3828f6f5e22
winterguard <aliasbonfire@gmail.com>
2020-11-19 21:30:58 +0900

    Profile 수정

---
+++ pages/profile/index.js
@@ -0,0 +1,29 @@
+const About = () => {
+  return (
+    <div>
+      <h2>Profile</h2>
+      <div>
+        <h3>Skills</h3>
+        <dl>
+          <dt>
+            <div>HTML/CSS/Javascript Image</div>
+            <strong>HTML/CSS/Javascript</strong>
+          </dt>
+          <dd></dd>
+          <dt>
+            <div>VueJS Image</div>
+            <strong>VueJS + NuxtJS</strong>
+          </dt>
+          <dd></dd>
+          <dt>
+            <div>IMG</div>
+            <strong>React</strong>
+          </dt>
+          <dd></dd>
+        </dl>
+      </div>
+    </div>
+  );
+};
+
+export default About;
