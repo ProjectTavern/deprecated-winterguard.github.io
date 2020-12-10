085d06494049fc69dcaf3aacbdaa72700faea4dc
winterguard <aliasbonfire@gmail.com>
2020-12-10 20:20:29 +0900

    adjust menu position to sticky

--- layouts/MainMenu/private.MainMenu.styled.js
+++ layouts/MainMenu/private.MainMenu.styled.js
@@ -2,10 +2,13 @@ import styled from "styled-components";
 
 const StyledMenu = styled.menu`
   display: flex;
+  position: sticky;
+  top: -1px;
   padding: 0 32px;
   border-top: 1px solid #d5d5d5;
   border-bottom: 1px solid #d5d5d5;
   overflow: scroll;
+  background-color: #f1f2f3;
 `;
 const StyledMenuItem = styled.li`
   a {
