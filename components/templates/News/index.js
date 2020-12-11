00c82ad34ff6de68f64ed5e8f3495d45f7298228
winterguard <aliasbonfire@gmail.com>
2020-12-11 23:35:27 +0900

    css news template

--- components/templates/News/index.js
+++ components/templates/News/index.js
@@ -1,10 +1,16 @@
 import { PostCard } from "@/components/organizations";
 
-import { StyledNews } from "./private.News.styled";
+import { StyledNews, StyledNewsMain } from "./private.News.styled";
 
 const News = ({ children }) => {
   return (
     <StyledNews>
+      <StyledNewsMain>
+        <h3>Brooklyn Nine-Nine Season2 Available!</h3>
+        <div className="news-main">
+          <img src="https://www.thenerddaily.com/wp-content/uploads/2020/01/You-Should-Watch-Brooklyn-Nine-Nine.jpg" />
+        </div>
+      </StyledNewsMain>
       <ul>
         {new Array(5).fill(0).map((element, index) => (
           <li key={index}>
--- components/templates/News/private.News.styled.js
+++ components/templates/News/private.News.styled.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
 const StyledNews = styled.section`
+  width: 100%;
+
   ul {
     display: grid;
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(4, 1fr);
 
     li {
       padding: 4px;
@@ -11,4 +13,39 @@ const StyledNews = styled.section`
   }
 `;
 
-export { StyledNews };
+const StyledNewsMain = styled.section`
+  position: relative;
+  margin: 4px;
+  margin-bottom: 40px;
+
+  h3 {
+    position: absolute;
+    left: 2rem;
+    bottom: 2rem;
+    font-size: 3rem;
+    color: #fff;
+    z-index: 100;
+    text-shadow: 1px 1px 5px #000;
+  }
+
+  .news-main {
+    position: relative;
+    overflow: hidden;
+    // padding-top: 56.25%;
+    padding-top: 40%;
+    cursor: pointer;
+  }
+
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+export { StyledNews, StyledNewsMain };
