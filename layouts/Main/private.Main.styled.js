61614fe3ed7fae85c5c57741c4989004501d5042
winterguard <aliasbonfire@gmail.com>
2020-12-11 22:16:15 +0900

    add main bottom padding

--- layouts/Main/private.Main.styled.js
+++ layouts/Main/private.Main.styled.js
@@ -1,13 +1,11 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const StyledMain = styled.main`
   min-height: 100vh;
-  padding: 2.5rem 1.5rem 0;
+  padding: 2.5rem 1.5rem 3rem;
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
-export {
-  StyledMain,
-};
+export { StyledMain };
